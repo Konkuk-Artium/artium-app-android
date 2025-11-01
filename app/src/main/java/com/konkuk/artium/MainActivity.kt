e3e010ab2e98bf606a51f58e59fb3f1348946f69
librawish808 <sophiadiary842@gmail.com>
2025-11-01 23:16:43 +0900

    Fix: 시스템바 영역 겹침 문제 수정

--- app/src/main/java/com/konkuk/artium/MainActivity.kt
+++ app/src/main/java/com/konkuk/artium/MainActivity.kt
@@ -3,45 +3,35 @@ package com.konkuk.artium
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.konkuk.artium.ui.feature.archive.screen.ArchiveScreen
 import com.konkuk.artium.ui.theme.ArtiumTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         setContent {
             ArtiumTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ArchiveScreen(
+                    onCardClick = {},
+                    onArrowClick = {}
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtiumTheme {
-        Greeting("Android")
+        ArchiveScreen(
+            onCardClick = {},
+            onArrowClick = {}
+        )
     }
 }
