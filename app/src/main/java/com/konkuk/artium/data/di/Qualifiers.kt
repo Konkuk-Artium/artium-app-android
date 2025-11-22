f3c710121e17561f2141622c9726fe9c8ca6a040
librawish808 <sophiadiary842@gmail.com>
2025-11-23 02:42:42 +0900

    Feat: Retrofit 구분용 Qualifier 추가 - 로그인용 네트워크와 일반 데이터용 네트워크를 구분하기 위해 Annotation 생성 - Hilt에서 두 종류의 Retrofit을 정확하게 주입할 수 있도록 준비

---
+++ app/src/main/java/com/konkuk/artium/data/di/Qualifiers.kt
@@ -0,0 +1,11 @@
+package com.konkuk.artium.data.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class LoginNetwork
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DataNetwork
